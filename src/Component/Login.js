c605b3d71bcaca74fac4369887e0b4007f205f75
kanika.np@acem.edu.in <kanikakumari.vacso@fynd-external.com>
2022-10-07 18:25:59 +0530

    updating data

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,20 +1,26 @@
-import {useDispatch} from "react-redux"
+import {useDispatch} from "react-redux";
+import emailjs from 'emailjs-com';
+
 function Login(){
     const dispatch = useDispatch();
     function handleLogin(event){
         event.preventDefault();
         let login_credential = event.target.elements.login_credential.value;
         let password = event.target.elements.password.value;
-        console.log(login_credential);
-        console.log(password);
-        console.log("I am running");
         const requestOptions = {
           method: 'POST',
           headers: { 'Content-Type': 'application/json'},
           body: JSON.stringify({ user_credential: login_credential,
                                  password: password })
           };
-        
+          
+          emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', 'Hello', 'YOUR_USER_ID')
+          .then((result) => {
+              window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+          }, (error) => {
+              console.log(error.text);
+          });
+
           fetch('http://localhost:8080/api/login', requestOptions)
           .then((response) => response.json())
           .then((newdata) => dispatch({type:'LOGIN',payload:newdata}))
